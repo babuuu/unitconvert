0c5976f3da46dc2669d7401b7bf0affafa8b62d7
Daniel Babin <Daniel Babin>
2017-11-14 12:43:05 -0500

    actually works

--- unitconvert/distance.py
+++ unitconvert/distance.py
@@ -16,7 +16,7 @@ def miles_to_kilometers(miles):
     
     return(miles*1.60934)
 
-def celsius_to_fahrenheit(km):
+def kilometers_to_miles(km):
     '''
     CONVERTS KILOMETERS TO MILES
     
--- unitconvert/tests/test_temperature.py
+++ unitconvert/tests/test_temperature.py
@@ -1,15 +1,15 @@
 import pytest
 
-from unitconvert.temperature import celsius_to_fahrenheit, fahrenheit_to_celsius
+from unitconvert import temperature
 
 def test_temperature():
     # some known results
     # distance between two same points should be zero
-    assert fahrenheit_to_celsius(50) == 10
-    assert celsius_to_fahrenheit(10) == 50
+    assert temperature.fahrenheit_to_celsius(50) == 10
+    assert temperature.celsius_to_fahrenheit(10) == 50
     
     # now check that we can't pass the wrong number of arguments
     with pytest.raises(TypeError):
-        fahrenheit_to_celsius(1, 2)
+        temperature.fahrenheit_to_celsius(1, 2)
     with pytest.raises(TypeError):
-        celsius_to_fahrenheit(1, 2)       
+        temperature.celsius_to_fahrenheit(1, 2)       
